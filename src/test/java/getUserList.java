982b68ce5ca81a51e277e7c9b08de70ef80e2617
neha1412 <nehakum287@gmail.com>
2022-01-21 15:17:07 +0530

    Rest Assured_2nd assignment

---
+++ src/test/java/getUserList.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class getUserList {
+    @Test
+    public void getAllUser(){
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
+}
